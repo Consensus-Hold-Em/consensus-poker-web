169596bd6962081224bcd502f3609d0097d6ba3f
Aaron Landy <aaronlan95@gmail.com>
2024-05-31 04:01:30 -0500

    Custom terminal reset text on change

--- components/custom-terminal.tsx
+++ components/custom-terminal.tsx
@@ -26,7 +26,8 @@ export const CustomTerminal = ({streamText}) => {
 
   // function to simulate typing
   const simulateTyping = (text: string, delay = 50) => {
-    text += '\n'
+    setAutoTypeText(''); // clear the text within the emulator
+    text += ' '
     let index = 0;
     const typingInterval = setInterval(() => {
       if (index < text.length) {
